177de02abd39d373e137cab061c849f349c995a2
DanielRLOo <dnielrl007@gmail.com>
2022-05-24 06:39:14 -0300

    Saving...

--- courses/6_js4backend/nodetest.js
+++ courses/6_js4backend/nodetest.js
@@ -3,13 +3,30 @@ const client = {
   age: 25,
   id: 123456,
   pocketMoney: 50,
-  email: 'email@domain.com',
-  payBy: function (ammount) {
-    return ammount >= this.pocketMoney ? 'Card' : 'Money';
+  contact: {
+    knownEmails: [
+      'email@domain.com',
+      'anotheremail@anotherdomain.com'
+    ],
+    knownPhoneNumbers: [
+      '01555444',
+      '551234567'
+    ],
   },
   presentation: function () {
-    console.log(`I'm ${this.name} and my email address is ${this.email}`);
+    console.log(`I'm ${this.name} and my email address is ${this.contact.knownEmails[0]}`);
   }
 }
 
-client.presentation();
+const generateRel = (object) => {
+  let rel = '';
+  for (let key in object) {
+    if (typeof object[key] === 'object' || typeof object[key] === 'function') {
+      continue
+    } else { rel += `${key}: ${object[key]}\n`; }
+  }
+  console.log(rel);
+}
+
+//generateRel(client);
+console.table(client.contact.knownEmails);
